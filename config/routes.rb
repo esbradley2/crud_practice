3164b5d2ebbf95f9e14b04f89d97873b08856aa6
Arjun Venkataswamy <arjun@example.com>
2015-02-05 14:01:38 -0600

    finished tasks 1 and 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get("/", {:controller => "favorites", :action => "home"})
+  get("/favorites/:id", {:controller => "favorites", :action => "show"})
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
