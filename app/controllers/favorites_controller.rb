d7896ddf4fed769d772319aae256aeb7ffbb6a7d
Arjun Venkataswamy <arjun@example.com>
2015-02-05 15:55:36 -0600

    finished tasks 6 - 9

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -6,4 +6,51 @@ class FavoritesController < ApplicationController
   def show
     @favorite = Favorite.find(params['id'])
   end
+
+  def new
+  end
+
+  def create
+    f = Favorite.new
+    f.name = params['name']
+    f.url = params['url']
+    f.save
+
+    redirect_to "/favorites/#{f.id}"
+  end
+
+  def edit
+    @favorite = Favorite.find(params['id'])
+  end
+
+  def update
+    f = Favorite.find(params['id'])
+    f.name = params['name']
+    f.url = params['url']
+    f.save
+
+    redirect_to "/favorites/#{f.id}"
+  end
+
+  def destroy
+    f = Favorite.find(params['id'])
+    f.destroy
+
+    redirect_to "/favorites/new"
+  end
+
+  def index
+    @favorites = Favorite.all
+  end
 end
+
+
+
+
+
+
+
+
+
+
+
